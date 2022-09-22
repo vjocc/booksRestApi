a3d713c559485a1508304a4913db5f160788777f
Jocc <vincze.joci@gmail.com>
2022-09-22 21:23:11 +0200

    add book post to api

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 require('dotenv').config()
+const booksRoute = require('./routes/books')
 
 const PORT = process.env.PORT || 3000
+
+//middleware
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+//routes
+app.use('/api/books', booksRoute)
 
 //connect to mongodb atlas
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser:true}).then(() => {
