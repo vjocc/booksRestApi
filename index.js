48b89e005294748c9fd695cd59b7d7e1974bd7d9
Jocc <vincze.joci@gmail.com>
2022-09-22 23:11:33 +0200

    added winston log

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const winston = require('winston')
 const app = express()
 require('dotenv').config()
 const booksRoute = require('./routes/books')
@@ -10,17 +11,33 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+//create a logger
+const logger = winston.createLogger({
+  level: 'info',
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize({all:true})
+      )
+    })
+  ],
+  exceptionHandlers: [
+    new winston.transports.File({ filename: 'exceptions.log' })
+  ]
+});
+
 //routes
 app.use('/api/books', booksRoute)
 
 //connect to mongodb atlas
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser:true}).then(() => {
-  console.log("Connected to mongodb atlas")
+  logger.info("Connected to mongoDb atlas")
 }).catch(error => {
-  console.log("mongodb error: ", error)
+  logger.error(error.message)
 })
 
 //start server
 app.listen(PORT, () => {
-  console.log("Server started at PORT ", PORT)
+  logger.warn(`Server started at PORT ${PORT}`)
 })
--- routes/books.js
+++ routes/books.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const express = require('express')
 const router = express.Router()
 const { Book, validateBook } = require('../models/books')
@@ -23,6 +24,48 @@ router.post('/', async (req, res) => {
     res.status(500).send("Book was not stored in db")
   })
 
+})
+
+//get all books
+router.get('/', (req, res) => {
+  Book.find()
+    .then((books) => res.send(books))
+    .catch(error => {
+      res.status(500).send("Something went wrong")
+    })
+})
+
+//get the book by id
+router.get('/:bookId', async (req, res) => {
+  const book = await Book.findById(req.params.bookId)
+  if(!book) res.status(404).send("Book not found")
+  res.send(book)
+})
+
+//update book based on id
+router.put('/:bookId', async (req, res) => {
+  const updatedBook = await Book.findByIdAndUpdate(
+    req.params.bookId, 
+    {
+      name: req.body.bookName,
+      author: {
+        name: req.body.authorName,
+        age: req.body.authorAge
+      },
+      genre: req.body.genre
+    },
+    { new:true }
+  )
+
+  if(!updatedBook) res.status(404).send("Book not found")
+  res.send(updatedBook)
+})
+
+//delete book based on id
+router.delete('/:bookId', async (req, res) => {
+  const book = await Book.findByIdAndRemove(req.params.bookId)
+  if(!book) res.status(404).send("Book not found")
+  res.send(book)
 })
 
 module.exports = router
